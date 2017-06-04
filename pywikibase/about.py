fbfc3522846d62ad77d586d1bf7066262f8e3378
Amir Sarabadani <ladsgroup@gmail.com>
2017-06-04 05:25:16 +0430

    Remove extra comma in about.py
    It caused failure when uploading it to PyPI
    
    Bug: T166781
    Change-Id: Ibb71a1641ff19d29a10fb0ba0509b0c2973363e0

--- pywikibase/about.py
+++ pywikibase/about.py
@@ -1,6 +1,6 @@
 __name__ = 'pywikibase'
 __version__ = '0.0.5'
-__maintainer__ = 'The Pywikibot team',
+__maintainer__ = 'The Pywikibot team'
 __maintainer_email__ = 'pywikibot@lists.wikimedia.org',
 __description__ = "Python package to handle Wikibase DataModel"
 __license__ = 'MIT License'
